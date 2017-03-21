4c8c814824085485bcc35c11853b9267acfd1223
danembarnett <danembarnett@gmail.com>
2017-03-21 00:25:28 -0400

    question4

---
+++ src/hackerrank/Question4.java
@@ -0,0 +1,18 @@
+package hackerrank;
+import java.util.Scanner;
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int i = scan.nextInt();
+		double d = scan.nextDouble();
+		String s = scan.next();
+		
+		System.out.println(i);
+		System.out.println(d);
+		System.out.println(s);
+	}
+
+}
